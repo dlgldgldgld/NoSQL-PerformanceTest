bb0a5bbf39787a03875fe34e3e16d89456bc4a9f
dlgldgld <shin12272014@gmail.com>
2022-03-17 21:54:17 +0900

    adding testcase_gen.py.

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
 # NoSQL-PerformanceTest
 NoSQL 종류별 performance 분석을 해보자!
+
+## TestCase 추출
+일단 NoSQL 별로 Test 측정을 해볼 데이터 Set부터 설계해보자.  
+공부 중인 도서에 의하면 Log나 상품 구매정보가 예시로 많이 나왔었다.  
+그 중 상품 구매정보에 대해서 DB에 저장을 한다는 가정하에 데이터를 생성해보자.
+
+Schema는 아래와 같이 정해보았다.
+
+### Market Sell - Schema
+
+|Column|Column(eng)|Description|Type|범위|
+|---|---|---|---|---|
+|고객 id|user_id|고객의 id|text|uuid_4|
+|고객 성명|user_name|고객의 이름|text|-|
+|회원/비회원|is_member|고객이 회원인지 비회원인지?|boolean|0 = 비회원, 1 = 회원|
+|성별|sex|고객의 성별|boolean|0 = 여성, 1 = 남성|
+|나이|age|고객의 나이|short|12~70|
+|직업|job|고객의 직업에 대한 정보|text|-|
+|주소|home_address|배송이 이뤄져야할 위치에 대한 정보|text|-|
+|접속 IP|ip_address|ipv4|text|public ipv4|
+|상품코드|product_id|어떤 상품을 구매했는지?|text|`[a-zA-Z]{1}_[0-9]{2}`, 총 5200개|
+|구매개수|sell_cnt|제품을 몇개 구입했는지 조사|short|1-30|
+|구매시간|sell_time|Timestamp|timestamp|2010.03.05 ~ 현재|
+|구매경로|web_or_mobile|어느 경로로 구매하였는지?|text|['Web', 'Mobile']|
+|구입/선물|sell_or_gift|구입 혹은 선물을 한건지?|boolean|0 = 구입, 1 = 선물|
+
+**requirements**
+ - 현재 마켓의 고객은 총 10,000명으로 가정.
+ - 마켓의 Open 날짜는 2010년 3월 5일로 구매 시간은 2010.03.05 ~ 현재까지로 구성.
+ - 총 1,000 만개의 record 생성.
+ - 고객 성명은 중복될 수 있음.
+
+<br> 
+
+다음으로 NoSQL의 종류별로 사용되는 Source 파일을 선택해보자.
+csv, json을 사용할 예정이며 사용해볼 소스파일의 format은 다음과 같이 정하였다.
+
+|Database|source file|
+|----|----|
+|RDBMS(SQLite)|csv|
+|분산 KVS(DynamoDB)|csv, JSON|
+|WCS(Apache Cassandra)|csv, JSON|
+|Document store(MongoDB)|JSON|
+|검색 엔진(ElasticSearch)|JSON|
---
+++ testcase/testcase_gen.py
@@ -0,0 +1,64 @@
+from collections import namedtuple
+from faker import Faker
+from faker.providers import internet
+from datetime import datetime
+from concurrent.futures import ProcessPoolExecutor, as_completed
+
+import math
+import os
+import csv
+
+STORE_OPEN_DATE = datetime(2010, 3, 5)
+ALL_RECORD_COUNT = 10000000
+
+fake = Faker('ko_KR')
+fake.add_provider(internet)
+
+client_info = namedtuple('client_info', 
+               ['user_id', 'user_name', 'is_member', 'sex', 'age', 
+                'job', 'home_address', 'ip_address'])
+
+trade_info = namedtuple('trade_info', 
+              ['product_id', 'sell_cnt', 'sell_time', 'web_or_mobile', 'sell_or_gift'])
+
+CSV_HEDAER = list(client_info._fields)
+CSV_HEDAER.extend(trade_info._fields)
+
+def gen_client() :
+    for _ in range(0, 10000):
+        yield client_info(
+            fake.unique.uuid_4(), fake.name(), fake.pybool(), fake.pybool(), fake.random.randint(12, 70),
+            fake.job(), fake.unique.address(), fake.unique.ipv4_public())
+
+def gen_loginfo( user_list, max_cnt = 100 ) :
+    for _ in range(max_cnt):
+        user = fake.random_element(elements = user_list)
+        trade = trade_info(
+                fake.bothify(text='?-##'), fake.random.randint(1, 30),
+                fake.date_time_between_dates(datetime_start=STORE_OPEN_DATE, datetime_end = datetime.now()),
+                fake.random_element(['Web', 'Mobile']), fake.pybool())
+        yield ( user, trade )
+
+def generate_logfile( user_list : list[(client_info, trade_info)], file_name : str, process_cnt : int , record_cnt = 10000 ) :
+    loginfo = gen_loginfo(user_list, record_cnt)
+    f_name = "".join([file_name, '_', str(process_cnt), '.csv'])
+    with open( f_name , 'w', newline='') as f:
+        w = csv.writer(f)
+        w.writerow(CSV_HEDAER)
+
+        for user, trade in loginfo :
+            record = list(user)
+            record.extend(trade)
+            w.writerow(record)
+
+if __name__ == "__main__" :
+    filename = 'test_file_name'
+
+    cpu_cnt = os.cpu_count()
+    user_list = list(gen_client())
+    per_write_record = math.ceil( ALL_RECORD_COUNT / cpu_cnt )
+
+    with ProcessPoolExecutor() as e:
+        futures = [e.submit(generate_logfile, user_list, filename, num, per_write_record) for num in range (cpu_cnt)]
+        for future in as_completed(futures):
+            future.result()
