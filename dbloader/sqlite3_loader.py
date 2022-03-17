67c0a45b3e59299e39b7d502b7232558fc9a0d29
dlgldgld <shin12272014@gmail.com>
2022-03-18 03:03:16 +0900

    add sqlite3_loader.py

---
+++ dbloader/sqlite3_loader.py
@@ -0,0 +1,19 @@
+import sqlite3
+import pandas
+import locale
+import time
+import os
+
+table_name = "log_info"
+dbname = 'example.db'
+
+os.remove(dbname)
+start = time.perf_counter()
+conn = sqlite3.connect(dbname)
+for count in range (0, 6):
+    csvfile = f'test_file_name_{count}.csv'
+    df = pandas.read_csv(csvfile, encoding=locale.getpreferredencoding())
+    df.to_sql(table_name, conn, if_exists='append', index=False)
+
+end = time.perf_counter()
+print(f'[SQLITE3]-FILE LOADER : {end - start:0.4f} seconds')
