7dff79111e1a643742b40a3d010cf3d0af10b4ed
dlgldgld <shin12272014@gmail.com>
2022-03-18 17:41:21 +0900

    divide logfile to user_info and trade_info.

--- README.md
+++ README.md
@@ -3,22 +3,30 @@ NoSQL 종류별 performance 분석을 해보자!
 
 ## TestCase 추출
 일단 NoSQL 별로 Test 측정을 해볼 데이터 Set부터 설계해보자.  
-공부 중인 도서에 의하면 Log나 상품 구매정보가 예시로 많이 나왔었다.  
-그 중 상품 구매정보에 대해서 DB에 저장을 한다는 가정하에 데이터를 생성해보자.
+첫째로는 Key-Value의 장점을 확인하기 위한 유저 정보 list에 대해서 Schema를 구성해보자.
 
 Schema는 아래와 같이 정해보았다.
 
-### Market Sell - Schema
+### USER 정보 - Schema
 
 |Column|Column(eng)|Description|Type|범위|
 |---|---|---|---|---|
 |고객 id|user_id|고객의 id|text|uuid_4|
+|고객 pw|user_pw|고객의 pw|text|12자리 알파벳 + 숫자의 조합|
 |고객 성명|user_name|고객의 이름|text|-|
 |회원/비회원|is_member|고객이 회원인지 비회원인지?|boolean|0 = 비회원, 1 = 회원|
 |성별|sex|고객의 성별|boolean|0 = 여성, 1 = 남성|
 |나이|age|고객의 나이|short|12~70|
 |직업|job|고객의 직업에 대한 정보|text|-|
 |주소|home_address|배송이 이뤄져야할 위치에 대한 정보|text|-|
+
+그리고 웹 서비스를 통해 상품을 판매하고 있다는 것을 가정하여 판매 정보에 대해서도 미리 Schema를 정해두자.
+
+### 판매 정보 - Schema
+
+|Column|Column(eng)|Description|Type|범위|
+|---|---|---|---|---|
+|고객 id|user_id|고객의 id|text|uuid_4|
 |접속 IP|ip_address|ipv4|text|public ipv4|
 |상품코드|product_id|어떤 상품을 구매했는지?|text|`[a-zA-Z]{1}_[0-9]{2}`, 총 5200개|
 |구매개수|sell_cnt|제품을 몇개 구입했는지 조사|short|1-30|
@@ -26,15 +34,7 @@ Schema는 아래와 같이 정해보았다.
 |구매경로|web_or_mobile|어느 경로로 구매하였는지?|text|['Web', 'Mobile']|
 |구입/선물|sell_or_gift|구입 혹은 선물을 한건지?|boolean|0 = 구입, 1 = 선물|
 
-**requirements**
- - 현재 마켓의 고객은 총 10,000명으로 가정.
- - 마켓의 Open 날짜는 2010년 3월 5일로 구매 시간은 2010.03.05 ~ 현재까지로 구성.
- - 총 1,000 만개의 record 생성.
- - 고객 성명은 중복될 수 있음.
-
-<br> 
-
-다음으로 NoSQL의 종류별로 사용되는 Source 파일을 선택해보자.
+다음으로는 NoSQL의 종류별로 Test 가능한 Source 파일을 정해보자.
 csv, json을 사용할 예정이며 사용해볼 소스파일의 format은 다음과 같이 정하였다.
 
 |Database|source file|
@@ -44,3 +44,38 @@ csv, json을 사용할 예정이며 사용해볼 소스파일의 format은 다�
 |WCS(Apache Cassandra)|csv, JSON|
 |Document store(MongoDB)|JSON|
 |검색 엔진(ElasticSearch)|JSON|
+
+**TEST 1. RDBMS vs WCS 비교 - 유저 정보 Read / Write 시간 비교**
+처음으로 해볼 것은 RDBMS와 WCS를 비교해본다.  
+좀 더 엄밀히 말하자면 Key-Value Store와 RDBMS 사이에 어떠한 차이가 있는지에 대해서 비교를 해보고자 한다.
+
+실시간 웹 서비스에서 유저의 정보를 빠르게 받아오고 가져오는 상황이 일어났다고 하자.  
+첫번째로 알아보고 싶은 것은 실제 Write 속도가 얼마나 차이가 있을지에 대한 차이다.  
+
+아래의 테스트 환경으로 진행해보자.
+
+**Environments**
+- RDBMS = SQLITE3( Local ), WCS = Cassandra( Local )
+  - 서비스를 진행 중인 상태를 가정할 것이기 때문에 SQLITE3에서는 Index를 미리 만들어 놓기로 한다. ( Write 속도는 느려지나, Read 속도는 빨라짐 )
+- 총 1,000만 명의 유저정보가 있다고 가정하고 해당 정보를 각 DB에 저장.
+- 유저정보에 관한 Schema는 위에서 정의한 Schema와 동일.
+- Primary Key는 user_id 이다.
+
+
+**TEST 2. Requirement**
+ - 현재 마켓의 고객은 총 10,000명으로 가정.
+ - 마켓의 Open 날짜는 2010년 3월 5일로 구매 시간은 2010.03.05 ~ 현재까지로 구성.
+ - 총 1,000 만개의 record 생성.
+ - 고객 성명은 중복될 수 있음.
+
+<br> 
+
+
+### Cassandra
+- Docker에 올려서 하는건 csv import 에러가 발생.
+- Local에 받아서 사용하는건 import 하는 것이 너무 느림.
+- apache-cassandra-3.11.12 기준 python 2.7로 사용 가능.
+
+실행 방법 
+1. Cassandra 실행 - C:\apache\apache-cassandra-3.11.12\bin\cassandra
+2. cqlsh 실행 - C:\apache\apache-cassandra-3.11.12\bin\cqlsh
--- testcase/testcase_gen.py
+++ testcase/testcase_gen.py
@@ -11,24 +11,29 @@ import csv
 STORE_OPEN_DATE = datetime(2010, 3, 5)
 ALL_RECORD_COUNT = 10000000
 
-fake = Faker('ko_KR')
+fake = Faker()
 fake.add_provider(internet)
 
-client_info = namedtuple('client_info', 
-               ['user_id', 'user_name', 'is_member', 'sex', 'age', 
-                'job', 'home_address', 'ip_address'])
+client_info = namedtuple(
+    'client_info', ['user_id', 'user_pw', 
+    'user_name', 'is_member', 'sex', 
+    'age', 'job', 'home_address', 'ip_address'])
 
-trade_info = namedtuple('trade_info', 
-              ['product_id', 'sell_cnt', 'sell_time', 'web_or_mobile', 'sell_or_gift'])
+trade_info = namedtuple(
+    'trade_info', ['product_id', 'sell_cnt', 
+    'sell_time', 'web_or_mobile', 'sell_or_gift'])
 
-CSV_HEDAER = list(client_info._fields)
-CSV_HEDAER.extend(trade_info._fields)
+CSV_HEADER = list(client_info._fields)
+CSV_HEADER.extend(trade_info._fields)
 
-def gen_client() :
-    for _ in range(0, 10000):
+def gen_client( user_cnt = 10000 ) :
+    for _ in range(0, user_cnt):
+        address = fake.unique.address()
+        address = address.replace("\n", " ")
         yield client_info(
-            fake.unique.uuid4(), fake.name(), fake.pybool(), fake.pybool(), fake.random.randint(12, 70),
-            fake.job(), fake.unique.address(), fake.unique.ipv4_public())
+            fake.unique.uuid4(), fake.password(length=12, upper_case=True), fake.name(), 
+            fake.pybool(), fake.pybool(), fake.random.randint(12, 70),
+            fake.job(), address, fake.unique.ipv4_public())
 
 def gen_loginfo( user_list, max_cnt = 100 ) :
     for _ in range(max_cnt):
@@ -39,26 +44,61 @@ def gen_loginfo( user_list, max_cnt = 100 ) :
                 fake.random_element(['Web', 'Mobile']), fake.pybool())
         yield ( user, trade )
 
-def generate_logfile( user_list : list[(client_info, trade_info)], file_name : str, process_cnt : int , record_cnt = 10000 ) :
+def generate_clientfile( 
+    process_cnt : int, record_cnt : int, 
+    file_name :str, header : list[str] ):
+    """generate client information to csv file.
+
+    Args:
+        process_cnt (int): if this function will be executed by multiprocess, using this args for distinguishing process number.
+        file_name (_type_): result file name
+        header (list[str]): result csv file header list.
+        record_cnt (int): records count.
+    """
+    generator = gen_client(record_cnt)
+    outfilename = "".join([file_name, "_", str(process_cnt), ".csv"])
+    with open( outfilename, 'w', newline='', encoding='utf-8') as f:
+        w = csv.writer(f)
+        w.writerow(header)
+        for client in generator:
+            w.writerow(client)
+
+def generate_logfile( 
+    process_cnt : int, record_cnt : int,
+    file_name : str, header : list[str], user_list : list[client_info]) :
+    """Generate combine user_info with trade_info. and It will convert to CSV file.
+
+    Args:
+        process_cnt (int): if this function will be executed by multiprocess, using this args for distinguishing process number.
+        user_list (list[client_info]): user_information
+        file_name (str): csv file name
+        header (list[str]): csv file header
+        record_cnt (int, optional): log file records count. Defaults to 10000.
+    """
     loginfo = gen_loginfo(user_list, record_cnt)
     f_name = "".join([file_name, '_', str(process_cnt), '.csv'])
-    with open( f_name , 'w', newline='') as f:
+    with open( f_name , 'w', newline='', encoding='utf-8') as f:
         w = csv.writer(f)
-        w.writerow(CSV_HEDAER)
+        w.writerow(header)
 
         for user, trade in loginfo :
             record = list(user)
             record.extend(trade)
             w.writerow(record)
 
-if __name__ == "__main__" :
-    filename = 'test_file_name'
-
+def multiprocess_start( f , *args ) :
     cpu_cnt = os.cpu_count()
-    user_list = list(gen_client())
     per_write_record = math.ceil( ALL_RECORD_COUNT / cpu_cnt )
 
     with ProcessPoolExecutor() as e:
-        futures = [e.submit(generate_logfile, user_list, filename, num, per_write_record) for num in range (cpu_cnt)]
+        futures = [e.submit(f, num, per_write_record, *args ) for num in range (cpu_cnt)]
         for future in as_completed(futures):
             future.result()
+
+if __name__ == "__main__" :
+    filename = 'test_file_name'
+    # client info
+    generate_clientfile(0, ALL_RECORD_COUNT, filename, list(client_info._fields) )
+    # log info
+    # user_list = list(gen_client())
+    # multiprocess_start( generate_logfile, filename, CSV_HEADER, user_list )
